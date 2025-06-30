7bea67ddc0f1c6c4846c66d03dfca9c504d0a03a
jerryCh254 <jerrych254@gmail.com>
2025-06-30 12:38:50 +0500

    fvdfvvdvf

--- src/webhook/webhook.module.ts
+++ src/webhook/webhook.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WebhookController } from './webhook.controller';
 import { WebhookService } from './webhook.service';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   controllers: [WebhookController],
   providers: [WebhookService],
 })
